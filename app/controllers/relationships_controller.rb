80de6c83c42da83d96535657679c35fb2d001a96
NamBker <namnv@runsystem.net>
2017-04-27 18:13:50 +0700

    finish follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,7 @@
 class RelationshipsController < ApplicationController
-	before_action :logged_in_user
-
 	def create
 		@user = User.find(params[:followed_id])
 		current_user.follow(@user)
-		buybug
 		respond_to do |format|
 			format.html { redirect_to @user }
 			format.js
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 
 puts "Create 5 User of TEAM"
 User.create!(username: "admin111",
-             email: "admin@gmail.com",
-             password:              "admin1",
-             password_confirmation: "admin1")
+ email: "admin@gmail.com",
+ password:              "admin1",
+ password_confirmation: "admin1")
 User.create!(username:  "namnguyen",
-             email: "nam@gmail.com",
-             password:              "123456",
-             password_confirmation: "123456")
+ email: "nam@gmail.com",
+ password:              "123456",
+ password_confirmation: "123456")
 User.create!(username:  "hiennguyen",
-             email: "hien@gmail.com",
-             password:              "123456",
-             password_confirmation: "123456")
+ email: "hien@gmail.com",
+ password:              "123456",
+ password_confirmation: "123456")
 User.create!(username:  "ducnguyen",
-             email: "duc@gmail.com",
-             password:              "123456",
-             password_confirmation: "123456")
+ email: "duc@gmail.com",
+ password:              "123456",
+ password_confirmation: "123456")
 
 User.create!(username:  "longtran",
-             email: "long@gmail.com",
-             password:              "123456",
-             password_confirmation: "123456")
+ email: "long@gmail.com",
+ password:              "123456",
+ password_confirmation: "123456")
 
 
 puts "Faker 50 Food"
@@ -34,22 +34,26 @@ users = User.order(:created_at).take(6)
   	price: Faker::Number.decimal(2, 3),
   	file:Faker::Avatar.image("my-own-slug", "50x50"),
   	) }
-  end
+end
 
-# puts "Creat foods"
-
-# # Microposts
-# users = User.order(:created_at).take(6)
-# 50.times do
-#   content = Faker::Lorem.sentence(5)
-#   users.each { |user| user.foods.create!(content: content) }
+# puts "create 20 user "
+# 20.times do |n|
+#   name  = Faker::Name.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(username:  name,
+#    email: email,
+#    password:              password,
+#    password_confirmation: password,
+#    )
 # end
+
 
 # Following relationships
 users = User.all
 user  = users.first
-following = users[2..5]
-followers = users[3..4]
+following = users[2..10]
+followers = users[3..15]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
