5b6040eaf0013710757e70cdc11e6af85134bd7a
NamBker <namnv@runsystem.net>
2017-05-04 17:12:29 +0700

    doing

--- app/controllers/newsfeed_controller.rb
+++ app/controllers/newsfeed_controller.rb
@@ -2,5 +2,6 @@ class NewsfeedController < ApplicationController
 	def index
 		@user  = User.find(current_user.id)
 		@users = User.all
+		@foods = Food.all.order('created_at DESC').page params[:page]
 	end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,19 @@
-class RelationshipsController < ApplicationController
-	def create
+
+			format.html { redirect_to @user }
+			format.html { redirect_to @user }
+			format.js
+			format.js
+		@user = Relationship.find(params[:id]).followed
 		@user = User.find(params[:followed_id])
 		current_user.follow(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
-	end
-
-	def destroy
-		@user = Relationship.find(params[:id]).followed
 		current_user.unfollow(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
 		end
+		end
+		respond_to do |format|
+		respond_to do |format|
+	def create
+	def destroy
 	end
+	end
+class RelationshipsController < ApplicationController
 end
